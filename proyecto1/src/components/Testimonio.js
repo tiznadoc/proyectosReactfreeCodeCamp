4919d48770ef7efd95e6e51724bd31a42953b66c
tiznadoc <tiznadoc@gmail.com>
2023-11-29 17:20:43 -0300

    testing

---
+++ proyecto1/src/components/Testimonio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <div className="testimonio-container">
+      <img 
+      className="testimonio-image"
+      src={require("../images/testimonio-Emma.png")}
+      alt="Emma's image"
+      />
+    </div>
+  );
+}
