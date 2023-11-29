a5e4ac5fb5ac51f3d0b400d06835a268a6995be1
tiznadoc <tiznadoc@gmail.com>
2023-11-29 18:23:28 -0300

    Added Testimonio compenent, js and css files. App.js edited

--- proyecto1/src/components/Testimonio.js
+++ proyecto1/src/components/Testimonio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../stylesheets/Testimonio.css';
 
 function Testimonio() {
   return (
@@ -8,6 +9,13 @@ function Testimonio() {
       src={require("../images/testimonio-Emma.png")}
       alt="Emma's image"
       />
+      <div className="testimonio-text-container">
+        <p className="testimonio-name">Emma Bostian in Sweden</p>
+        <p className="testimonio-job">Software Engineer at Spotify</p>
+        <p className="testimonio-text">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+      </div>
     </div>
   );
 }
+
+export default Testimonio;
