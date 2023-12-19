d3b7dec0698a9250048fb9f0787d36a16075940e
tiznadoc <tiznadoc@gmail.com>
2023-12-19 00:32:53 -0300

    app.css edited

--- proyecto2/src/App.js
+++ proyecto2/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-
+      <div className='freecodecamp-logo-container'>
+        <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freecodecamp' />
+      </div>
     </div>
   );
 }
