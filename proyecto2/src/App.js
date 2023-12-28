a009eb2c26f475bcc080222fc6be66b4a6d3edae
tiznadoc <tiznadoc@gmail.com>
2023-12-28 11:20:58 -0300

    app.js. edited

--- proyecto2/src/App.js
+++ proyecto2/src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="app">
       <div className='freecodecamp-logo-container'>
         <img 
           className='freecodecamp-logo'
