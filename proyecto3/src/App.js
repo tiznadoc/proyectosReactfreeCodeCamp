b1e0f634af0426b67953216244a32a1390e0f35d
tiznadoc <tiznadoc@gmail.com>
2024-01-09 18:35:39 -0300

    button component added

--- proyecto3/src/App.js
+++ proyecto3/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp-logo-container'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='freeCodeCamp logo'
+        />
+      </div>
+      <div className='calculator-container'>
+        <div className='row'>
+          <Button>1</Button>
+        </div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+      </div>
     </div>
   );
 }
---
+++ proyecto3/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button(props) {
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
+}
+
+export default Button
