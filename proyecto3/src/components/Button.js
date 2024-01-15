d93c70549e5710c1622e0269b71016ef08beb7b1
tiznadoc <tiznadoc@gmail.com>
2024-01-15 12:49:49 -0300

    some dependencies fixed & more button compenents added

--- proyecto3/src/components/Button.js
+++ proyecto3/src/components/Button.js
@@ -1,18 +1,23 @@
 import React from "react";
+import '../style-sheets/Button.css'
 
+// This function takes in a prop called 'children' and returns a div element with a className based on the value of the 'children' prop.
 function Button(props) {
 
+    // This function checks if the value of the 'children' prop is not a number, and if it is not equal to '.' or '='
     const isOperator = value => {
         return isNaN(value) && (value !== '.') && (value !== '=');
     };
 
+    // Return a div element with a className based on the value of the 'children' prop
     return (
         <div 
-        className={`button-container ${isOperator(props.children) ? 'operator' : null}`}
+        className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}
         >
         {props.children}
         </div>
     )
 }
 
+// Export the Button function so it can be used in other components
 export default Button;
