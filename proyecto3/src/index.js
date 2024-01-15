c3e90c472e9d41eadef8a1018610d1ed8305ce59
tiznadoc <tiznadoc@gmail.com>
2024-01-15 17:56:15 -0300

    index.js saved

--- proyecto3/src/index.js
+++ proyecto3/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Create a root element in the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Render the App component into the root element
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
